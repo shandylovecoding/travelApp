77cf8382af9f38adedffe89fee966103c73f3bbe
shandylovecoding <shandylovecoding@gmail.com>
2021-07-30 21:41:03 +0800

    login and sign up page done

--- migrations/20210729122353_users.js
+++ migrations/20210729122353_users.js
@@ -1,8 +1,8 @@
 exports.up = function(knex) {
     return knex.schema.createTable("users", (table) => {
         table.increments('id').primary();
-        table.string("username")
         table.string("email").unique();
+        table.string("username")
         table.string("password");
         table.timestamps(false,true) 
 })
